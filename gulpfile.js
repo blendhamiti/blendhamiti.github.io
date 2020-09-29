cbc0c70dfc43dbafea28fe7e8ff339df2519a8cf
Blend Hamiti <b.hamiti.9@gmail.com>
2020-09-29 02:38:58 +0200

    implemented gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,66 @@
+const { watch } = require('gulp');
 const gulp = require('gulp');
 const fileinclude = require('gulp-file-include');
 
-async function includeHTML() {
-    return gulp.src([
-        './src/*.html',
-        '!./src/header.html',
-        '!./src/footer.html',
-        '!./src/navbar.html'
-    ])
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(gulp.dest('./'));
-};
+// async function includeHTML() {
+//     return gulp.src([
+//         './src/*.html',
+//         '!./src/header.html',
+//         '!./src/footer.html',
+//         '!./src/navbar.html'
+//     ])
+//         .pipe(fileinclude({
+//             prefix: '@@',
+//             basepath: '@file'
+//         }))
+//         .pipe(gulp.dest('./'));
+// };
 
-exports.default = includeHTML;
+// watch([
+//     './src/*.html',
+//     './src/css/*.css',
+//     './src/js/*.js'
+// ], includeHTML());
+
+// async function includeHTML() {
+
+//     watch([
+//         './src/*.html',
+//         './src/css/*.css',
+//         './src/js/*.js'
+//     ], function cb() {
+//         return gulp.src([
+//             './src/*.html',
+//             '!./src/header.html',
+//             '!./src/footer.html',
+//             '!./src/navbar.html'
+//         ])
+//             .pipe(fileinclude({
+//                 prefix: '@@',
+//                 basepath: '@file'
+//             }))
+//             .pipe(gulp.dest('./'));
+//     });
+// };
+
+// exports.default = includeHTML;
+
+exports.default = function () {
+    watch([
+        './src/*.html',
+        './src/css/*.css',
+        './src/js/*.js'
+    ], function cb() {
+        return gulp.src([
+            './src/*.html',
+            '!./src/header.html',
+            '!./src/footer.html',
+            '!./src/navbar.html'
+        ])
+            .pipe(fileinclude({
+                prefix: '@@',
+                basepath: '@file'
+            }))
+            .pipe(gulp.dest('./'));
+    });
+};
