e0b69dbc6a8489e029368b6c613329697d568aba
Blend Hamiti <b.hamiti.9@gmail.com>
2020-10-24 14:04:02 +0200

    js fix and skills footnote

--- src/js/main.js
+++ src/js/main.js
@@ -54,6 +54,32 @@ var resumeIconsContainers = document.querySelectorAll(".icon-block span")
 resumeIconsContainers.forEach(container => {
     container.title = container.childNodes.item(0).classList[0].split("-", "2")[1];
 });
+window.addEventListener('load', function () {
+    var components = document.querySelectorAll('.component');
+    var navLinks = document.querySelectorAll(".nav-link");
+
+    // Get any component's position to use the global x, y, positon values
+    var pagePosition = components.item(0).getBoundingClientRect();
+
+    var componentsHeights = [0];
+    var componentsHeightsSum = 0;
+
+    components.forEach(component => {
+        componentsHeightsSum += component.getBoundingClientRect().height;
+        componentsHeights.push(-componentsHeightsSum + (window.screen.height / 2));
+    });
+
+    navLinks.forEach(function (navLink, index) {
+        if (pagePosition.y <= componentsHeights[index] && pagePosition.y > componentsHeights[index + 1]) {
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                if (link.name === components.item(index).id) {
+                    link.classList.add("active");
+                }
+            });
+        }
+    });
+});
 window.addEventListener('scroll', function () {
     var components = document.querySelectorAll('.component');
     var navLinks = document.querySelectorAll(".nav-link");
