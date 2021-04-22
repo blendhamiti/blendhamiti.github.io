46d5e66fc5fefb0e06efb5897ba113819d6f1860
Blend Hamiti <b.hamiti.9@gmail.com>
2021-04-22 23:28:51 +0200

    added styles for profile card 2

--- src/pages/profileCard.js
+++ src/pages/profileCard.js
@@ -10,38 +10,40 @@ import styles from "./profileCard.module.scss";
 
 function ProfileCard() {
     return (
-        <div id='profile-card' className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.pic}>
-                    <img
-                        src='/assets/images/portrait.jpg'
-                        width='160px'
-                        height='160px'
-                    />
-                </div>
-                <div className={styles.title}>
-                    <h1>Hi, I am Blend!</h1>
-                </div>
-                <div className={styles.description}>
-                    <p>
-                        I am an Engineering Technology graduate who has an
-                        interest in programming. This webiste is meant to
-                        showcase my skills and experience.
-                    </p>
-                </div>
-                <div className={styles.links}>
-                    <Icon
-                        link='https://github.com/blendhamiti/'
-                        icon={faGithub}
-                    />
-                    <Icon
-                        link='https://www.linkedin.com/in/blendhamiti/'
-                        icon={faLinkedin}
-                    />
-                    <Icon
-                        link='https://www.instagram.com/blendhamiti/'
-                        icon={faInstagram}
-                    />
+        <div className={styles.parent}>
+            <div id='profile-card' className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.pic}>
+                        <img
+                            src='/assets/images/portrait.jpg'
+                            width='160px'
+                            height='160px'
+                        />
+                    </div>
+                    <div className={styles.title}>
+                        <h1>Hi, I am Blend!</h1>
+                    </div>
+                    <div className={styles.description}>
+                        <p>
+                            I am an Engineering Technology graduate who has an
+                            interest in programming. This webiste is meant to
+                            showcase my skills and experience.
+                        </p>
+                    </div>
+                    <div className={styles.links}>
+                        <Icon
+                            link='https://github.com/blendhamiti/'
+                            icon={faGithub}
+                        />
+                        <Icon
+                            link='https://www.linkedin.com/in/blendhamiti/'
+                            icon={faLinkedin}
+                        />
+                        <Icon
+                            link='https://www.instagram.com/blendhamiti/'
+                            icon={faInstagram}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
