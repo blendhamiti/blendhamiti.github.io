7e69627da48a81ec16c12094dbdd22f4f0833a62
Blend Hamiti <b.hamiti.9@gmail.com>
2021-05-13 15:50:01 +0200

    started fixing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import clsx from 'clsx';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 
 import * as styles from './Navbar.module.scss';
 
@@ -9,45 +9,37 @@ function Navbar() {
   const [collapse, setCollapse] = React.useState(false);
 
   return (
-    <nav className={styles.nav}>
+    <nav>
       <div>
-        {/* <button onClick={() => setCollapse(!collapse)}>Toggler</button> */}
-        {/* <a href='#'>
-                    <img
-                        src='/assets/images/logo.png'
-                        width='30'
-                        height='30'
-                        alt='logo'
-                    />
-                </a> */}
-        <div>
-          <ul>
-            <li>
-              <a href="#profile-card">Introduction</a>
-            </li>
-            <li>
-              <a href="#timeline">Timeline</a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#certificates">Certificates</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-            <li>
-              <a href="/resume.pdf" className={styles.resume}>
-                Resume <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </li>
-          </ul>
-        </div>
+        <button onClick={() => setCollapse(!collapse)}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
       </div>
+      <ul>
+        <li>
+          <a href="#profile-card">Introduction</a>
+        </li>
+        <li>
+          <a href="#timeline">Timeline</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#certificates">Certificates</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="/resume.pdf" className={styles.resume}>
+            Resume <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
