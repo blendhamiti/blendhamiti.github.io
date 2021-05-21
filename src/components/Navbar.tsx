02286de801112d9e62c1b03d3d5999f104433851
Blend Hamiti <b.hamiti.9@gmail.com>
2021-05-21 22:12:29 +0200

    added tsconfig and reverted navbar handlers

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,20 @@
-import React, { FC, useRef, useState } from 'react';
-import clsx from 'clsx';
+import React, { FC } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 
 import * as styles from './Navbar.module.scss';
 
 const Navbar: FC<{}> = () => {
-  const [collapse, setCollapse] = useState(true);
-  const navRef = useRef<HTMLElement>(null);
-
-  useState(() => {
-    document.addEventListener('click', (event) => handleDropdownClick(event));
-    return () =>
-      document.removeEventListener('click', (event) =>
-        handleDropdownClick(event)
-      );
-  });
-
-  const handleDropdownClick = (event) => {
-    if (
-      navRef.current.contains(event.target) &&
-      event.target.localName === 'a'
-    ) {
-      setCollapse(true);
-    } else if (navRef.current.contains(event.target)) {
-    } else {
-      setCollapse(true);
-    }
-  };
+  const [collapse, setCollapse] = React.useState(false);
 
   return (
-    <nav ref={navRef}>
-      <div className={styles.container}>
-        <button
-          className={styles.toggleButton}
-          onClick={() => setCollapse(!collapse)}
-        >
+    <nav>
+      <div>
+        <button onClick={() => setCollapse(!collapse)}>
           <FontAwesomeIcon icon={faBars} />
         </button>
       </div>
-      <ul
-        className={clsx(styles.links, !collapse && styles.showDropdown)}
-        onClick={(event) => handleDropdownClick(event)}
-      >
+      <ul>
         <li>
           <a href="#profile-card">Introduction</a>
         </li>
